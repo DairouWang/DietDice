55d6198267ed26280be8482d196bc429cce5779a
Dairou Wang <dairou2021@gmail.com>
2024-07-10 20:14:01 -0700

    Initial Commit

---
+++ DietDice/DietDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietDiceApp.swift
+//  DietDice
+//
+//  Created by Darol on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DietDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
