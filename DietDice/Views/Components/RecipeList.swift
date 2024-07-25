115ecef4ec053d66d9d41e7051ab40fe3abec1cd
Dairou Wang <dairou2021@gmail.com>
2024-07-24 22:09:27 -0700

    add home view

---
+++ DietDice/Views/Components/RecipeList.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeList.swift
+//  DietDice
+//
+//  Created by Darol on 7/17/24.
+//
+
+import SwiftUI
+
+struct RecipeList: View {
+    var recipes:[Recipe]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(recipes.count) \(recipes.count > 1 ? "recipes" : "recipe")")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                .opacity(0.7)
+                Spacer()
+            }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160),spacing: 15)], spacing: 15) {
+                ForEach(recipes) {
+                    recipe in
+                    RecipeCard(recipe: recipe)
+                }
+            }
+            .padding(.top)
+        }.padding(.horizontal)
+    }
+}
+
+#Preview {
+    ScrollView{
+        RecipeList(recipes: Recipe.all)
+    }
+}
--- DietDice/Views/Main/HomeView.swift
+++ DietDice/Views/Main/HomeView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            List(Recipe.all) { recipe in
-                Text(recipe.name)
-                    .navigationTitle("My Recipes")
+            ScrollView{
+                RecipeList(recipes : Recipe.all)
             }
+            .navigationTitle("My Recipes")
         }
+        .navigationViewStyle(.stack)
     }
+    
 }
 
 #Preview {
